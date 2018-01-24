6f3063e41108e407ce116a3803c0ef22938bfae8
wiky <376447842@qq.com>
2018-01-24 18:32:31 +0800

    投影到推荐的

--- app/src/main/java/com/hulian/huliantecdemo/hook/hookutils/HookActivityManagerServiceUtils.java
+++ app/src/main/java/com/hulian/huliantecdemo/hook/hookutils/HookActivityManagerServiceUtils.java
@@ -2,6 +2,8 @@ package com.hulian.huliantecdemo.hook.hookutils;
 
 import android.content.Context;
 
+import java.lang.reflect.Field;
+
 /**
  * Created by wiky_zhang on 2018/1/24.
  * 创建一个钩子hook
@@ -24,5 +26,21 @@ public class HookActivityManagerServiceUtils {
         this.context = context;
     }
 
+    public void hookAms() throws Exception{
 
+       Class<?> cl= Class.forName("android.app.ActivityManagerNative");
+        Field d_field=cl.getDeclaredField("gDefault");
+        d_field.setAccessible(true);
+        Object d_value=d_field.get(null);//静态方法，获取gDefault的变量值
+        //反射singleton
+       Class<?> sclass= Class.forName("android.util.Singleton");
+        Field s_field=sclass.getDeclaredField("mInstance");
+        s_field.setAccessible(true);
+        //这个就是系统的IActivityManager的对象
+        Object s_value=s_field.get(d_value);
+
+
+
+
+    }
 }
